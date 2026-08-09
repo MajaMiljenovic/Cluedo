9df8b7020ed267311a6d20cbf2e14c35a82556ec
MajaMiljenovic <majamiljenovic@outlook.com>
2026-08-09 02:34:39 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,10 @@
-const CACHE="cluedo-v18";
+const CACHE="cluedo-v19";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-180.png","./icon-512.png"];
 
 self.addEventListener("install", event => {
   self.skipWaiting();
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
 });
-
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys()
@@ -13,7 +12,6 @@ self.addEventListener("activate", event => {
       .then(() => self.clients.claim())
   );
 });
-
 self.addEventListener("fetch", event => {
   if(event.request.mode==="navigate"){
     event.respondWith(
@@ -27,8 +25,5 @@ self.addEventListener("fetch", event => {
     );
     return;
   }
-
-  event.respondWith(
-    caches.match(event.request).then(cached=>cached||fetch(event.request))
-  );
+  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request)));
 });
